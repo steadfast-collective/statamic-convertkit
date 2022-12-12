f8fe1bd7f7210ff4c56e0b6a66f466e3e8380bff
James Pigott <jmpigott96@gmail.com>
2022-12-12 08:32:06 +0000

    Init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SteadfastCollective\Convertkit;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
