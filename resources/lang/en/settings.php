681757b69e0fe04168b5dc54a27ce95b007bb986
James Pigott <jmpigott96@gmail.com>
2022-12-12 15:39:10 +0000

    translations

---
+++ resources/lang/en/settings.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'api-token' => [
+        'title' => "API Key",
+        'instruction' => "Add your ConvertKit API Key to your `.env` file with the key `CONVERTKIT_KEY`. Your API key can be found in __*Account Settings -> Advanced*__",
+    ],
+    'forms' => [
+        'title' => "Forms",
+        'instruction' => "Enable ConvertKit integration for the following forms:"
+    ],
+    'field_mapping' => [
+        'title' => "Field Mapping",
+        'form_title' => "Field mapping for ':name'",
+        'convertkit_field' => "ConvertKit Field",
+        'form_field' => "Form Field",
+        "add_field" => "Add Field",
+        'custom_value' => "Custom Value",
+        'custom_field_key' => "Custom Field Name"
+    ]
+];
