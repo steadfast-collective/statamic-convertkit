8f8106555b825fa86072dc17d8808af69270b829
Duncan McClean <duncan@steadfastcollective.com>
2023-07-27 15:07:13 +0100

    wip

--- src/Http/Controllers/CP/SettingsController.php
+++ src/Http/Controllers/CP/SettingsController.php
@@ -5,13 +5,14 @@ namespace SteadfastCollective\ConvertKit\Http\Controllers\CP;
 use Statamic\Facades\Site;
 use Statamic\Http\Controllers\CP\CpController;
 use SteadfastCollective\ConvertKit\Facades\ConvertKitStorage;
+use SteadfastCollective\ConvertKit\SettingsBlueprint;
 
 class SettingsController extends CpController
 {
     public function index()
     {
         $data = $this->getData();
-        $blueprint = $this->getBlueprint();
+        $blueprint = SettingsBlueprint::getBlueprint();
         $fields = $blueprint->fields()->addValues($data)->preProcess();
 
         $key = config('convertkit.key');
@@ -27,7 +28,7 @@ class SettingsController extends CpController
 
     public function store(\Illuminate\Http\Request $request)
     {
-        $blueprint = $this->getBlueprint();
+        $blueprint = SettingsBlueprint::getBlueprint();
 
         $fields = $blueprint->fields()->addValues($request->all());
         $fields->validate();
