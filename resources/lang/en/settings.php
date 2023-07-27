c9c5e4a901e89eb79a739c769804c0b7fbabd5f4
Duncan McClean <duncan@steadfastcollective.com>
2023-07-27 15:22:19 +0100

    I dont *think* this component is used anywhere

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -4,14 +4,4 @@ return [
         'title' => "Forms",
         'instruction' => "Enable ConvertKit integration for the following forms:"
     ],
-    'field_mapping' => [
-        'title' => "Field Mapping",
-        'form_title' => "Field mapping for ':name'",
-        'convertkit_field' => "ConvertKit Field",
-        'form_field' => "Form Field",
-        "add_field" => "Add Field",
-        'custom_value' => "Custom Value",
-        'custom_field_key' => "Custom Field Name",
-        'http_referer' => "HTTP Referer",
-    ]
 ];
